5e7a70bea4c550528cc27da34dc845b6ff7359be
Rachel Alberts <rd.alberts@outlook.com>
2020-04-15 18:39:55 +0200

    Enemy follows the player when in range. Enemy has smooth collision with the enviorment.

---
+++ GB-Zelda/Assets/Scripts/KnockBack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+{
+    public float Thrust; //knock back force
+    
+    public float KnockTime;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("enemy"))
+        {
+            Rigidbody2D enemyRigidbody2D = other.GetComponent<Rigidbody2D>();
+            if(enemyRigidbody2D != null)
+            {
+                Vector2 difference =  enemyRigidbody2D.transform.position - transform.position;
+                difference = difference.normalized * Thrust;
+                enemyRigidbody2D.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockCo(enemyRigidbody2D));
+            }
+        }
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D enemy)
+    {
+        if (enemy != null)
+        {
+            yield return new WaitForSeconds(KnockTime);
+            enemy.velocity = Vector2.zero;
+        }
+    }
+}
---
+++ GB-Zelda/Assets/Scripts/Log.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : Enemy
+{
+    public Transform Target;
+    public float ChaseRadius; // radius log will chase after player
+    public float AttackRadius; //radius log will attack player
+    public Transform homePosition; // start/return position
+
+    public Animator animator;
+
+    private Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator =  GetComponent<Animator>();
+        Target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    private void CheckDistance()
+    {
+        if (Vector3.Distance(Target.position, transform.position) <= ChaseRadius && Vector3.Distance(Target.position, transform.position) > AttackRadius) 
+        {
+            Vector3 temp = Vector3.MoveTowards(transform.position, Target.position, MoveSpeed * Time.deltaTime);
+            rigidbody2D.MovePosition(temp);
+        }
+    }
+}
