9f8153a79b6a51930fa377abd4b8f85a7a29269b
Rachel Alberts <rd.alberts@outlook.com>
2020-04-15 13:02:36 +0200

    Player moves. Player has working walking animations. Player doens't look weird anymore when below objects.

---
+++ GB-Zelda/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed; //how fast the player moves
+    private Rigidbody2D rigidbody2d;
+    private Vector3 change; //change player position
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2d= GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
+         //if the player is moving
+        if(change != Vector3.zero)
+        {
+            MoveCharacter();
+            //change the idle animation
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    //move the character
+    void MoveCharacter()
+    {
+        rigidbody2d.MovePosition(
+            transform.position + change.normalized * speed * Time.deltaTime);
+    }
+
+
+}
